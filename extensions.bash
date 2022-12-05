a02027aeecf996bfb7e116d9d8e76dc16b1bf453
Chris du Plessis <christopherjonduplessis@gmail.com>
2022-12-05 20:50:45 +0200

    Installed VS code

--- extensions.bash
+++ extensions.bash
@@ -3,3 +3,4 @@
 code --install-extension julialang.language-julia
 code --install-extension github.vscode-pull-request-github
 code --install-extension rust-lang.rust-analyzer
+code --install-extension GitHub.github-vscode-theme
