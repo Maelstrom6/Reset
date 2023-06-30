8c4b21a1f10b5e610906f7e54a838aeb9da4c5c0
Chris du Plessis <45897212+Maelstrom6@users.noreply.github.com>
2023-06-30 20:20:44 +0200

    Update nvidia.bash

--- nvidia.bash
+++ nvidia.bash
@@ -13,9 +13,21 @@ nvidia-smi  # Should show your graphics card
 # toolkit 11.2
 # https://developer.nvidia.com/cuda-toolkit-archive
 # https://developer.nvidia.com/cuda-11.2.2-download-archive?target_os=Linux&target_arch=x86_64&target_distro=Ubuntu&target_version=2004&target_type=runfilelocal
-wget https://developer.download.nvidia.com/compute/cuda/11.2.2/local_installers/cuda_11.2.2_460.32.03_linux.run
-sudo sh cuda_11.2.2_460.32.03_linux.run --toolkit --silent --override
+# wget https://developer.download.nvidia.com/compute/cuda/11.2.2/local_installers/cuda_11.2.2_460.32.03_linux.run
+# sudo sh cuda_11.2.2_460.32.03_linux.run --toolkit --silent --override
 # Test installation
+nvcc -V  # Should show the version of cuda toolkit
+
+# wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin
+# sudo mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
+# wget https://developer.download.nvidia.com/compute/cuda/12.2.0/local_installers/cuda-repo-ubuntu2204-12-2-local_12.2.0-535.54.03-1_amd64.deb
+# sudo dpkg -i cuda-repo-ubuntu2204-12-2-local_12.2.0-535.54.03-1_amd64.deb
+# sudo cp /var/cuda-repo-ubuntu2204-12-2-local/cuda-*-keyring.gpg /usr/share/keyrings/
+# sudo apt-get update
+# sudo apt-get -y install cuda
+
+# Just do this
+sudo apt install nvidia-cuda-toolkit
 nvcc -V  # Should show the version of cuda toolkit
 
 # Add some environment variables
