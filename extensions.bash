478b0ba25caf587d25d4d18e9a2ad2d0e93a103b
Chris du Plessis <christopherjonduplessis@gmail.com>
2023-07-29 16:33:04 +0200

    Updated python installs

--- extensions.bash
+++ extensions.bash
@@ -4,3 +4,4 @@ code --install-extension julialang.language-julia
 code --install-extension github.vscode-pull-request-github
 code --install-extension rust-lang.rust-analyzer
 code --install-extension GitHub.github-vscode-theme
+code --install-extension platformio.platformio-ide
--- python.bash
+++ python.bash
@@ -1,3 +1,5 @@
 sudo apt install python3-pip -y
-
+sudo apt-get install python-is-python3
+sudo apt install python3-distutils
+sudo apt install python3-venv
 pip install virtualenv
